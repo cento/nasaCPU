e40bff9f83249956256748bfc296a338bc2c1074
A <cento@users.noreply.github.com>
2023-11-20 23:19:15 +0800

    Removed all projects from old vitis/vivado

---
+++ Vitis_workspace/fetching_ip/fetching_ip/hls/.autopilot/db/kernel_module_hierarchy.tcl
@@ -0,0 +1,9 @@
+set ModuleHierarchy {[{
+"Name" : "fetching_ip","ID" : "0","Type" : "sequential",
+"SubLoops" : [
+	{"Name" : "VITIS_LOOP_20_1","ID" : "1","Type" : "no",
+	"SubInsts" : [
+	{"Name" : "grp_fetch_fu_62","ID" : "2","Type" : "sequential"},
+	{"Name" : "pc_1_execute_fu_69","ID" : "3","Type" : "sequential"},
+	{"Name" : "is_running_running_cond_update_fu_74","ID" : "4","Type" : "sequential"},]},]
+}]}
