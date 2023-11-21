80620869cac84cfc611cf8a491d698ce931c1353
A <cento@users.noreply.github.com>
2023-11-21 21:03:01 +0800

    Folder cleanup

--- Vitis_workspace/platform_fetching_ip/ps7_cortexa9_0/standalone_ps7_cortexa9_0/bsp/libsrc/build_configs/exmetadata/CMakeFiles/fetching_ip_example.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fetching_ip_example"
-  "xfetching_ip_exlist.yaml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fetching_ip_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
